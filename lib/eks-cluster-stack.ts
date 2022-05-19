3f65330ec73e5afc48dee774ee20083cd5df068b
Khanh Tran <ktran@cas.org>
2022-05-19 10:26:00 -0400

    fix vpc

--- lib/eks-cluster-stack.ts
+++ lib/eks-cluster-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import eks = require("aws-cdk-lib/aws-eks");
 import ec2 = require("aws-cdk-lib/aws-ec2");
 import { Construct } from 'constructs'
+import { Vpc, IVpc } from "aws-cdk-lib/aws-ec2"
 
 import { EksManagedNodeGroup } from "./infrastructure/eks-mng";
 
@@ -15,7 +16,12 @@ export class EksClusterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: EksClusterStackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, "Vpc", { maxAzs: 3 });
+    const vpc = Vpc.fromVpcAttributes(scope, 'default-vpc',
+    {
+      vpcId: '0d6840f719e623c0b',
+      availabilityZones: ['us-east-2a', 'us-east-2b', 'us-east-2c'],
+      privateSubnetIds: ["subnet-0ecdc7f883bd80731", "subnet-04426cd587ab2f852", "subnet-0e987c484ac30538d"],
+    })
 
     const cluster = new eks.Cluster(this, `kxt29-${props.nameSuffix}`, {
       clusterName: `kxt29-${props.nameSuffix}`,
@@ -25,10 +31,10 @@ export class EksClusterStack extends cdk.Stack {
       vpcSubnets: [{ subnetType: ec2.SubnetType.PRIVATE }],
     });
 
-    // const eksMng = new EksManagedNodeGroup(this, "EksManagedNodeGroup", {
-    //   cluster: cluster,
-    //   nameSuffix: props.nameSuffix,
-    // });
+    const eksMng = new EksManagedNodeGroup(this, "EksManagedNodeGroup", {
+      cluster: cluster,
+      nameSuffix: props.nameSuffix,
+    });
 
 
   }
